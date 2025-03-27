a84650a75af8de8b8294870963370dda76011275
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-03-27 08:37:44 -0300

    atualizacao

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function(){
  
  aumentaFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize= '${tamanhoAtualFonte}rem';
+    document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
 })
 const diminuirFonteBotao= document.getElementById('diminuir-fonte');
 diminuirFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize= '${tamanhoAtualFonte}rem';
+    document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
 })
 })
